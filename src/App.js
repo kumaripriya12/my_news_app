8aec4f591c1445e40b592d59ec820af87c988618
Priya <KPriya@ad.harman.com>
2020-07-17 08:40:26 +0530

    removed http calls

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './Components/Header'
-import MainBody from './Components/MainBody'
-import Category from './Components/Category'
-import Detail from './Components/Detail'
-import Video from './Components/Video'
-import VideoMeet from './Components/VideoMeet'
-import NotFound from './Components/NotFound'
-import Footer from './Components/Footer'
+ import Header from './Components/Header'
+ import VideoMeet from './Components/VideoMeet'
 
 function App() {
 
@@ -20,12 +14,9 @@ function App() {
 			<Header />
 
 			<Switch>
-				<Route exact path="/detail" component={Detail} />
-				<Route exact path="/video" component={Video} />
+				
 				<Route exact path="/videomeet" component={VideoMeet} />
-				<Route exact path="/:id" render={props => <Category key={props.match.params.id || 'empty'} id={props.match.params.id || 'empty'} /> } />
-				<Route exact path="/" component={MainBody} />		
-				<Route component={NotFound} />		
+				
 			</Switch>
 		</div>
 	</Router>
