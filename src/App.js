b5626424b39f300b926cf642be4ec9f7d0b4d9f9
Priya <KPriya@ad.harman.com>
2020-07-22 12:25:46 +0530

    Removed api calls

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './Components/Header'
-import MainBody from './Components/MainBody'
-import Category from './Components/Category'
-import Detail from './Components/Detail'
 import Video from './Components/Video'
 import VideoMeet from './Components/VideoMeet'
-import NotFound from './Components/NotFound'
-import Footer from './Components/Footer'
+
 
 function App() {
 
@@ -20,12 +16,9 @@ function App() {
 			<Header />
 
 			<Switch>
-				<Route exact path="/detail" component={Detail} />
 				<Route exact path="/video" component={Video} />
 				<Route exact path="/videomeet" component={VideoMeet} />
-				<Route exact path="/:id" render={props => <Category key={props.match.params.id || 'empty'} id={props.match.params.id || 'empty'} /> } />
-				<Route exact path="/" component={MainBody} />		
-				<Route component={NotFound} />		
+				
 			</Switch>
 		</div>
 	</Router>
